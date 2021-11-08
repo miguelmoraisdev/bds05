3fd71e2ad8ae1d4eb998c370987dcba1e9e16696
Miguel Morais <miguelmoraisdev@gmail.com>
2021-11-07 21:12:20 -0300

    Implementing endpoint profile

--- src/main/java/com/devsuperior/movieflix/services/UserService.java
+++ src/main/java/com/devsuperior/movieflix/services/UserService.java
@@ -3,13 +3,17 @@ package com.devsuperior.movieflix.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.movieflix.dto.UserDTO;
 import com.devsuperior.movieflix.entities.User;
 import com.devsuperior.movieflix.repositories.UserRepository;
+import com.devsuperior.movieflix.services.exceptions.UnauthorizedException;
 
 @Service
 public class UserService implements UserDetailsService{
@@ -18,6 +22,19 @@ public class UserService implements UserDetailsService{
 
 	@Autowired
 	private UserRepository repository;
+	
+	@Transactional(readOnly = true)
+	public UserDTO getProfileAuthenticated() {
+		try {
+			String username = SecurityContextHolder.getContext().getAuthentication().getName();
+			User user = repository.findByEmail(username);
+			return new UserDTO(user);
+		}
+		catch (Exception e) {
+			throw new UnauthorizedException("Invalid user");
+		}
+	}
+	
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
